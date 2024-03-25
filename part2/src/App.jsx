2f7f1f6a7e9ede0fb832f130388973f493ba1c8a
marco hernandez <mh94.design@gmail.com>
2024-03-25 13:53:09 -0600

    2.5: Separate module step10

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -48,9 +48,12 @@ const App = () => {
 
   return (
     <>
-    <h1>Web development curriculum</h1>
+      <h1>Web development curriculum</h1>
       {
-        courses.map(course => <Course course={course} key={course.id}/>)
+        courses.map(course => {
+          console.log('Course with the ID:', course.id, 'has the name:', course.name)
+          return <Course course={course} key={course.id} />
+        })
       }
     </>
   )
