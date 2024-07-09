c217ced79f671acbcbb5baebc30eb9dd637bd8b7
marco hernandez <mh94.design@gmail.com>
2024-07-09 12:25:52 -0600

    4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,9 +1,14 @@
 const dummy = (blogs) => {
-  // ...
   const res = blogs ? 1 : 0
   return res
 }
 
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return totalLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
