600e60c550b0520debeec4854f2643e956c2a3a3
marco hernandez <mh94.design@gmail.com>
2024-08-08 12:11:57 -0600

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,22 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  const showWhenVisible = { display: showInfo ? '' : 'none' }
+
+  const btnText = showInfo ? 'hide' : 'show'
+
+  return (
+    <div className='blog'>
+      <p>{blog.title} <button onClick={() => setShowInfo(!showInfo)}>{btnText}</button></p>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
