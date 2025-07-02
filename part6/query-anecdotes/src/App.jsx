6cc77ee70d146d56d8aebc044530680ebe6fe1ee
marco hernandez <mh94.design@gmail.com>
2025-07-02 14:30:24 -0600

    Exercise 6.20

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,54 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const queryClient = useQueryClient()
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+  console.log(data)
+
+  if (isPending) {
+    return <div>loading data...</div>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  const anecdotes = data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
